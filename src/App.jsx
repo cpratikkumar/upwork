92f9eda09107e8d1e19e4bc1eb25df964a2e5dd5
Pratik <iamcodery60@gmail.com>
2024-09-11 22:41:46 +0530

    ghv

--- src/App.jsx
+++ src/App.jsx
@@ -31,26 +31,26 @@ function App() {
   return (
     <div>
       <Navbar />
-      <div className='section-1 flex justify-around items-center px-52 mt-36 sm:flex-col md:flex-col lg:flex-row ' >
+      <div className='section-1 flex justify-around items-center px-52 mt-36 sm:flex-col md:flex-col lg:flex-row' >
         <div className='section-1-left lg:w-[70%] lg:h-[60%] lg:mr-5 flex flex-col justify-center md:w-[85vw] md:h-[60%] md:ml-5 sm:w-[85vw] sm:h-[60%] sm:ml-5' >
-          <div className=' rounded-2xl bg-gray-100 w-[100px] h-[30px] text-center text-sm font-normal mb-5 flex items-center justify-center font-jakarta md:ml-[0vw]'>Gantt Charts</div>
+          <div className=' rounded-2xl bg-gray-100 w-[100px] h-[30px] text-center text-sm font-normal mb-5 flex items-center justify-center font-jakarta md:ml-[30vw] lg:ml-[0vw] sm:ml-[30vw]'>Gantt Charts</div>
 
           <div>
-            <h1 className=' font-extrabold text-5xl mb-3 font-jakarta'>CRM Software for Real Estate</h1>
-            <p style={{ textAlign: "left" }} className=' font-medium text-black mb-5 font-jakarta md:font-semibold sm:font-semibold md:text-center sm:text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad aliquid eius officiis? Unde, nam praesentium? Natus voluptatum, nam, enim sunt expedita ut consectetur fugiat ipsam at nesciunt architecto sed nobis, repellat doloremque. Fugiat quo veritatis dolorem delectus numquam sint, sapiente obcaecati atque illo qui modi distinctio quae minus amet animi consequatur iure illum blanditiis in cumque eaque. Sed dolore voluptas recusandae expedita. Odit,quaerat.</p>
+            <h1 className=' font-extrabold text-5xl mb-3 md:text-[45px] font-jakarta'>CRM Software for Real Estate</h1>
+            <p  className=' font-medium text-black mb-5 font-jakarta md:font-semibold sm:font-semibold lg:text-left md:text-center sm:text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad aliquid eius officiis? Unde, nam praesentium? Natus voluptatum, nam, enim sunt expedita ut consectetur fugiat ipsam at nesciunt architecto sed nobis, repellat doloremque. Fugiat quo veritatis dolorem delectus numquam sint, sapiente obcaecati atque illo qui modi distinctio quae minus amet animi consequatur iure illum blanditiis in cumque eaque. Sed dolore voluptas recusandae expedita. Odit,quaerat.</p>
           </div>
 
 
-          <div className=' flex lg:justify-between md:ml-[0vw] sm:ml-[0vw] lg:items-center md:justify-between md:items-center sm:justify-between sm:items-center lg:w-[70%] mb-3 md:w-[50vw] sm:w-[50vw]'>
+          <div className=' flex lg:justify-between lg:ml-[0vw] md:ml-[20vw] sm:ml-[20vw] lg:items-center md:justify-between md:items-center sm:justify-between sm:items-center lg:w-[70%] mb-3 md:w-[50vw] sm:w-[50vw]'>
             <button className='rounded-lg bg-gradient-to-r from-indigo-400 to-cyan-400 lg:w-[250px] lg:h-[40px] md:w-[250px] md:h-[40px] sm:w-[250px] sm:h-[40px] text-white font-medium text-lg font-jakarta '>Get Started</button>
-            <div className=' w-[20vw] ml-2'>
+            <div className=' lg:w-[20vw] lg:ml-2 md:w-[20vw] md:ml-2 sm:w-[40vw] sm:ml-2'>
               <p className=' text-sm font-medium text-gray-700 font-jakarta'>Free forever.</p>
               <p className=' text-sm font-medium text-gray-700 font-jakarta'>No credit card.</p>
             </div>
           </div>
 
-          <div className=' flex justify-between items-center lg:w-[80%] md:w-[50%] sm:w-[60%] md:ml-[0vw] sm:ml-[0vw] md:mb-10 sm:mb-10'> <span className=' md:text-xs sm:text-xs'>⭐ ⭐ ⭐ ⭐ ⭐</span>
-            <p className=' font-jakarta text-cyan-400 flex lg:w-[20vw] md:w-[25vw] sm:w-[25vw] md:text-xs sm:text-xs md:items-center sm:items-center' >25,000+ reviews
+          <div className=' flex justify-between items-center lg:w-[80%] md:w-[50%] sm:w-[60%] lg:ml-[0vw] md:ml-[20vw] sm:ml-[20vw] md:mb-10 sm:mb-10'> <span className=' md:text-xs sm:text-xs'>⭐ ⭐ ⭐ ⭐ ⭐</span>
+            <p className=' font-jakarta text-cyan-400 flex lg:w-[20vw] md:w-[25vw] sm:w-[30vw] md:text-xs sm:text-[9px] md:items-center sm:items-center' >25,000+ reviews
               <img src="https://tse4.mm.bing.net/th?id=OIP.jHQLwTvVoKGcJNKN_5jL3QHaHa&pid=Api&P=0&h=180" alt="images" className=' lg:w-[20px] lg:h-[20px] ml-3 md:w-[15px] md:h-[15px] sm:w-[15px] sm:h-[15px]' />
               <img src="https://clipart.info/images/ccovers/1522453412Logo-Snapchat-Png.png" alt="images" className=' lg:w-[20px] lg:h-[20px] mx-3 md:w-[15px] md:h-[15px] sm:w-[15px] sm:h-[15px]' />
               <img src="https://tse1.mm.bing.net/th?id=OIP.EhTMbGiYfYzQnWLgjZaoJAHaHa&pid=Api&P=0&h=180" alt="images" className=' lg:w-[20px] lg:h-[20px] md:w-[15px] md:h-[15px] sm:w-[15px] sm:h-[15px]' />
@@ -61,27 +61,27 @@ function App() {
         </div>
         <div className='section-1-right lg:w-[50%] lg:h-[60%] ml-5 flex justify-center items-center rounded-xl md:w-[80vw] md:h-[60%]'>
 
-          <div className='images border-2 lg:w-[30vw] lg:h-[60vh] bg-white flex justify-center items-center rounded-xl shadow-slate-100 shadow-md md:w-[80vw] md:h-[60vh] sm:w-[30vw] sm:h-[60vh]'>
-            <img src={image1} alt="images1" className=' absolute z-0 w-[18%] h-[250px] -mt-10 ml-24 rounded-lg animate-moveHorizontal shadow-lg' />
-            <img src={image2} alt="images2" className=' absolute z-10 w-[18%] h-[250px] -mt-0 ml-18 rounded-lg animate-moveHorizontal shadow-lg' />
-            <img src={image3} alt="images3" className=' absolute z-20 w-[18%] h-[250px] mt-20 -ml-20 rounded-lg animate-moveHorizontal shadow-lg' />
+          <div className='images border-2 lg:w-[30vw] lg:h-[60vh] bg-white flex justify-center items-center rounded-xl shadow-slate-100 shadow-md md:w-[80vw] md:h-[60vh] sm:w-[80vw] sm:h-[60vh]'>
+            <img src={image1} alt="images1" className=' absolute z-0 lg:w-[18%] lg:h-[250px] md:w-[50%] md:h-[250px] sm:w-[50%] sm:h-[250px] -mt-10 ml-24 rounded-lg animate-moveHorizontal shadow-lg' />
+            <img src={image2} alt="images2" className=' absolute z-10 lg:w-[18%] lg:h-[250px] md:w-[50%] md:h-[250px] sm:w-[50%] sm:h-[250px] -mt-0 ml-18 rounded-lg animate-moveHorizontal shadow-lg' />
+            <img src={image3} alt="images3" className=' absolute z-20 lg:w-[18%] lg:h-[250px] md:w-[50%] md:h-[250px] sm:w-[50%] sm:h-[250px] mt-20 -ml-20 rounded-lg animate-moveHorizontal shadow-lg' />
 
           </div>
         </div>
       </div>
 
-      <div className='brand-promotion flex justify-between items-center my-24 px-52'>
-        <img src="https://logos-world.net/wp-content/uploads/2020/11/Logitech-Symbol.png" alt="brandImage" className=' w-[100px] h-[50px]' />
-        <img src="https://tse3.mm.bing.net/th?id=OIP.Z2fS0jPb8fBEFSdg26dR4gHaEK&pid=Api&P=0&h=180" alt="brandImage" className=' w-[100px] h-[50px]' />
-        <img src="https://tse3.mm.bing.net/th?id=OIP.BclrukCQrK_hFarUfmZXLQHaEK&pid=Api&P=0&h=180" alt="brandImage" className=' w-[100px] h-[50px]' />
-        <img src="https://purepng.com/public/uploads/large/purepng.com-ibm-logologobrand-logoiconslogos-251519939176ka7y8.png" alt="brandImage" className=' w-[100px] h-[50px]' />
-        <img src="https://tse3.mm.bing.net/th?id=OIP.Z2fS0jPb8fBEFSdg26dR4gHaEK&pid=Api&P=0&h=180" alt="brandImage" className=' w-[100px] h-[50px]' />
-        <img src="https://logos-world.net/wp-content/uploads/2020/11/Logitech-Symbol.png" alt="brandImage" className=' w-[100px] h-[50px]' />
+      <div className='brand-promotion flex lg:justify-between md:justify-between sm:justify-between items-center my-24 px-52 lg:w-[100vw] md:w-[100vw] sm:w-[100vw]'>
+        <img src="https://logos-world.net/wp-content/uploads/2020/11/Logitech-Symbol.png" alt="brandImage" className=' lg:w-[100px] lg:h-[50px] md:w-[50px] md:h-[50px] sm:w-[50px] sm:h-[50px] lg:mx-0 md:-ml-20 sm:-ml-32' />
+        <img src="https://tse3.mm.bing.net/th?id=OIP.Z2fS0jPb8fBEFSdg26dR4gHaEK&pid=Api&P=0&h=180" alt="brandImage" className=' lg:w-[100px] lg:h-[50px] md:w-[20px] md:h-[20px] sm:w-[50px] sm:h-[30px] lg:mx-0 md:mx-8 sm:mx-3' />
+        <img src="https://tse3.mm.bing.net/th?id=OIP.BclrukCQrK_hFarUfmZXLQHaEK&pid=Api&P=0&h=180" alt="brandImage" className=' lg:w-[100px] lg:h-[50px] md:w-[50px] md:h-[50px] sm:w-[50px] sm:h-[30px] lg:mx-0 md:mx-8 sm:mx-3' />
+        <img src="https://purepng.com/public/uploads/large/purepng.com-ibm-logologobrand-logoiconslogos-251519939176ka7y8.png" alt="brandImage" className=' lg:w-[100px] lg:h-[50px] md:w-[50px] md:h-[50px] sm:w-[50px] sm:h-[30px] lg:mx-0 md:mx-8 sm:mx-3' />
+        <img src="https://tse3.mm.bing.net/th?id=OIP.Z2fS0jPb8fBEFSdg26dR4gHaEK&pid=Api&P=0&h=180" alt="brandImage" className=' lg:w-[100px] lg:h-[50px] md:w-[50px] md:h-[50px] sm:w-[50px] sm:h-[30px] lg:mx-0 md:mx-4 sm:mx-3' />
+        <img src="https://logos-world.net/wp-content/uploads/2020/11/Logitech-Symbol.png" alt="brandImage" className=' lg:w-[100px] lg:h-[50px] md:w-[50px] md:h-[50px] sm:w-[50px] sm:h-[50px] lg:mx-0 md:-mr-12 sm:mr-18' />
       </div>
 
-      <div className='section-2 flex justify-between items-center px-36 mb-28' >
-        <div className=' w-[30vw] h-[45vh] bg-gradient-to-r from-cyan-400 to-violet-500 flex justify-center items-center rounded-xl ml-16 drop-shadow-2xl'><img src={image3} alt="images" className=' w-[30vw] h-[40vh] rounded-lg ml-10' /></div>
-        <div className='flex flex-col w-[20vw] h-[45vh] p-4 mr-80'>
+      <div className='section-2 flex justify-between items-center w-[100vw] px-36 mb-28' >
+        <div className='w-[25vw] h-[45vh]  bg-slate-400 bg-gradient-to-r from-cyan-400 to-violet-500 flex justify-center items-center rounded-xl ml-16 drop-shadow-2xl '><img src={image3} alt="images" className=' lg:w-[30vw] lg:h-[40vh] md:w-[50vw] md:h-[40vh] sm:w-[50vw] sm:h-[40vh] rounded-lg ml-10' /></div>
+        <div className='flex flex-col lg:m lg:w-[20vw] lg:h-[45vh] md:w-[30vw] md:h-[45vh] p-4 mr-80 bg-slate-400 lg:mr-[25vw] lg:ml-20 md:mr-[25vw] md:ml-0'>
           <div className=' w-[100px] h-[30px] bg-gray-100 rounded-xl text-center mb-2 -mt-4 font-jakarta flex justify-center items-center text-sm'>CRM Views</div>
           <h1 className=' font-bold text-3xl mb-3 font-jakarta'>See client relationships at a glance.</h1>
           <p className=' font-jakarta'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. perspiciatis nihil consequatur dolorum veniam voluptates, adipisci aut? Nobis?</p>
@@ -89,7 +89,7 @@ function App() {
 
       </div>
 
-      <div className=' w-[20vw] h-[30vh] p-4 text-left sticky top-24 right-10 ml-[70%] -mt-[55vh]'>
+      <div className=' w-[20vw] h-[30vh] p-4 text-left sticky top-24 right-10 ml-[70%] -mt-[55vh] md:hidden sm:hidden lg:block'>
         <h3 className=' font-bold text-base mb-4 font-jakarta'>Start using Pantheon Vista today</h3>
         <div className='flex justify-between items-center mb-4'><img src={checkmark} alt="tickmark" className=' w-[20px]' /><p className=' font-medium text-sm w-[100%] ml-4 font-jakarta'>Manage all your work in one place</p></div>
         <div className='flex justify-between items-center mb-4'><img src={checkmark} alt="tickmark" className=' w-[20px]' /><p className=' font-medium text-sm w-[100%] ml-4 font-jakarta'>Collaborate with your team</p></div>
@@ -98,13 +98,13 @@ function App() {
       </div>
 
 
-      <div className='section-3 w-[80vw] flex justify-evenly mb-24 mt-44'>
-        <div className=' w-[15vw] ml-14'>
-          <div className=' rounded-xl bg-gray-100 w-[100px] h-[30px] font-medium text-sm text-center mb-3 font-jakarta flex justify-center items-center'>Relationships</div>
+      <div className='section-3 w-[80vw] flex lg:justify-evenly mb-24 mt-44 lg:flex-row sm:flex-col md:flex-col'>
+        <div className=' lg:w-[15vw] md:w-[80vw] sm:w-[80vw] lg:ml-14 md:ml-24 sm:ml-24 mb-16'>
+          <div className=' rounded-xl bg-gray-100 w-[100px] h-[30px] font-medium text-sm text-center mb-3 font-jakarta flex justify-center items-center md:ml-60 sm:ml-40 lg:ml-0'>Relationships</div>
           <h1 className=' font-extrabold text-4xl mb-3 font-jakarta'> Build the perfect customer database.</h1>
-          <p className=' font-jakarta'>Lorem ipsum dolor sit amet consectetur adipisicing elit.  fugit nostrum corporis, neque, corrupti impedit.</p>
+          <p className=' font-jakarta lg:text-start md:text-center sm:text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. hsbdohsdovisudhnvlndboskvnakbd hvbiaksd viuasdv joajsdvoa sdv asdvaosdvoadv  dsoal dpsnoidvn vo  fugit nostrum corporis, neque, corrupti impedit.</p>
         </div>
-        <div className=' w-[30vw] h-[45vh] bg-gradient-to-r from-cyan-400 to-violet-500 flex justify-center items-center rounded-xl mr-16 drop-shadow-2xl'><img src={leadsWhite} alt="images" className=' w-[30vw] h-[40vh] rounded-lg mr-10' /></div>
+        <div className=' lg:w-[30vw] lg:h-[45vh] md:w-[70vw] md:h-[45vh] sm:w-[60vw] sm:h-[45vh] bg-gradient-to-r from-cyan-400 to-violet-500 flex justify-center items-center rounded-xl lg:mr-16 md:ml-36 sm:ml-36 drop-shadow-2xl'><img src={leadsWhite} alt="images" className=' lg:w-[30vw] md:w-[80vw] sm:w-[60vw] h-[40vh] rounded-lg lg:mr-10 md:-ml-10 sm:-ml-20' /></div>
       </div>
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import pantheonicon from "../../public/pantheonicon.png"
 function Navbar() {
   return (
     <div className='navbar-body w-[100vw] h-[100px] flex lg:justify-around lg:items-center md:justify-between md:items-center  mb-20 fixed top-0 z-50 sm:justify-between sm:px-10'>
-<div className='left-side-div lg:w-[20%] lg:h-[50px] rounded-xl border-[1px] md:w-[35%] md:h-[50px] md:ml-28 sm:w-[35%] sm:h-[50px] sm:mt-5 sm:-ml-0 border-gray-300 flex justify-around items-center bg-white shadow-slate-100 shadow-md'>
+<div className='left-side-div lg:w-[20%] lg:h-[50px] rounded-xl border-[1px] md:w-[35%] md:h-[50px] md:ml-10 lg:-ml-10 sm:w-[50%] sm:h-[50px] md:-mt-2 sm:mt-5 sm:-ml-0 border-gray-300 flex justify-around items-center bg-white shadow-slate-100 shadow-md'>
     <h1 className='font-extrabold text-xl font-jakarta text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-cyan-400'><img src={pantheonicon} alt="brandimage" className=' mt-4'/></h1> <span className='emptyline w-[1px] h-[30px] bg-black'></span><div><p className=' text-xs font-jakarta sm:text-[10px]'>The everything</p>
     <p className=' text-xs font-jakarta'>app for work.</p></div>
 </div>
-<div className='right-side-div-body lg:flex lg:w-[25%] h-[80px] justify-evenly items-center md:hidden'>
-   <div className='rounded-xl border-[1px] border-gray-300 w-[40%] h-[50px] flex items-center justify-center mr-5 bg-white shadow-slate-100 shadow-md sm:hidden md:hidden lg:block'><button className=' font-medium hover:bg-gray-100 w-[95%] h-[40px] rounded-lg font-jakarta'>Contact Sales</button></div>
+<div className='right-side-div-body lg:flex lg:w-[25%] h-[80px] justify-evenly items-center md:hidden lg:-mr-10'>
+   <div className='rounded-xl border-[1px] border-gray-300 w-[40%] h-[50px] flex items-center justify-center mr-5 bg-white shadow-slate-100 shadow-md sm:hidden md:hidden lg:block'><button className=' font-medium hover:bg-gray-100 w-[95%] h-[40px] m-1 rounded-lg font-jakarta'>Contact Sales</button></div>
    <div className='rounded-xl border-[1px] border-gray-300 w-[50%] h-[50px] flex items-center justify-evenly bg-white shadow-slate-100 shadow-md sm:hidden md:hidden lg:flex'>
     <button className=' font-medium hover:bg-gray-100 w-[50%] h-[40px] rounded-lg ml-1 font-jakarta'>Log in</button>
     <button className=' font-medium w-[50%] h-[40px] rounded-lg bg-gradient-to-r from-indigo-400 to-cyan-400 mx-1 font-jakarta'>Sign Up</button></div> 
     </div> 
 
     <div className='right-side-div-body flex w-[25%] h-[80px] justify-evenly  md:mr-8 items-center lg:hidden'>
-      <button className='rounded-xl bg-gradient-to-r from-indigo-400 to-cyan-400 mx-1 font-jakarta border-[1px] font-medium text-white w-[40%] h-[40px] mr-0 ml-2 sm:mr-5'>Sign Up</button>
-      <div className='rounded-xl w-[6vw] border-[1px] h-[40px] -ml-8'><img src="https://www.freeiconspng.com/uploads/menu-icon-6.png" alt="hamburger" className=' w-[50px] -mt-1 ml-1 sm:ml-0'/></div>
+      <button className='rounded-xl bg-gradient-to-r from-indigo-400 to-cyan-400 mx-1 font-jakarta border-[1px] font-medium text-white md:w-[40%] md:h-[40px] sm:w-[60%] sm:h-[40px] mr-0 ml-2 sm:mr-8'>Sign Up</button>
+      <div className='rounded-xl w-[6vw] border-[1px] h-[40px] -ml-8'><img src="https://www.freeiconspng.com/uploads/menu-icon-6.png" alt="hamburger" className=' w-[50px] h-[50px] sm:w-[50px] -mt-1 ml-1 sm:ml-0'/></div>
       </div>     
     </div>
   )
